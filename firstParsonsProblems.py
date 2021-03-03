57159cf4d157627536500a9a2718d412637a0a60
myla946 <79867480+myla946@users.noreply.github.com>
2021-03-03 11:11:50 -0700

    Add files via upload

---
+++ firstParsonsProblems.py
@@ -0,0 +1,38 @@
+"""
+March 2, 2021
+
+Use this document to record your answers to the Parson's Problems
+shared in class.
+
+Remember: the idea behind a Parson's Problem is that you are
+shown completely correct code that is presented OUT OF ORDER.
+When arranged correctly, the code does what the Parson's Problem
+says it should do.
+
+"""
+
+
+#Write your answer to Parson's Problem #1 below:
+#basic for loop
+for x in range(0,10):
+ print("hello world")
+
+
+
+#Write your answer to Parson's Problem #2 below:
+#basic list
+ myList = []
+myList.append("apples")
+myList.append("pineapples")
+print(myList)
+#Write your answer to Parson's Problem #3 below:
+else:
+#basic conditional
+x = input("Type a number")
+ x = int(x)
+if x > 0:
+   print("That're more than nothing !")
+else:
+   print("That's a small number !")
+
+
