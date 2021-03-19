660e1b5b51ec5740f64472f82ad75d4ee5b8cc61
myla946 <79867480+myla946@users.noreply.github.com>
2021-03-19 11:09:03 -0600

    Add files via upload

--- listAPCI.py
+++ listAPCI.py
@@ -1,7 +1,5 @@
 """
 programe goals:
-1. Add items to list(ints)
-2. Offer the user a choise of actions
 3. pull the values stored at specific indexes
 4. conver input to INTs
 5. put all action in a while loop
@@ -19,17 +17,20 @@ def mainProgram():
 2. Return a value in a list,
 3. Add a bunch
 4. random search,
-5. print list
-6. quit     """)
+5. Liner search
+6. print list
+7. quit     """)
         if choise == "1":
             addTolist()
         elif choise == "2":
-            indexValues
+            indexValues()
         elif choise == "3":
-            addAbunch
+            addAbunch()
         elif choise == "4":
             randomeSearch()
         elif choise == "5":
+              linerSearch()
+        elif choise == "6":
             print(myList)
         else:
             break
@@ -44,13 +45,20 @@ def addAbunch():
     numToAdd = input(" How many new inegers would you like to add?   ")
     numRange = input("And how high would you like these numbers to go?    ")
     for x in range(0, int(numToAdd)):
-        myList.append(random.randit(0,int(numRange)))
+        myList.append(random.randint(0,int(numRange)))
     print("Your list is complete!")
                 
 
 def randomSearch():
     print("Why doyou chosie this ")
     print(myList[random.randit(0, len(myList)-1)])
+
+def linerSearch():
+    print("WE'er going to go through this list one item at a time!")
+    searchValue = input("What are you looking for?    ")
+    for x in range(len(myList)):
+        if  myList[x] == int(searchValue):
+             print("Your item is at index postion  {}".format(x))
         
 
 def indexValues():
